6dcac4d17c73529da524669808598b4095d3f960
Salo Avalos <saloavalosc@gmail.com>
2020-10-13 19:32:46 -0500

    Correccin en diseño - Se acomodo Main activity por completo, se configuraron algunos colors y el style ,se empezo a corregir registro y login.

---
+++ app/src/main/java/com/example/proyectosamir/activities/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.proyectosamir.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.proyectosamir.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_main_signin;
+    private TextView tv_signin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_main_signin = findViewById(R.id.btn_main_signup);
+        tv_signin = findViewById(R.id.tv_signin);
+
+        btn_main_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup_activity = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(signup_activity);
+            }
+        });
+
+        tv_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            }
+        });
+    }
+
+}
